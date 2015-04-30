28e06d82f977073c56bdc57bdf8cf360085fd295
Ryan Miller <ryanmiller810@users.noreply.github.com>
2015-04-30 15:17:28 -0700

    Assign3.C

---
+++ Assignment3/Assign3.C
@@ -0,0 +1,21 @@
+#ifndef Assign3_CPP
+#define Assign3_CPP
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+
+  University CSUSM;
+  CSUSM.ProcessTransFile();
+
+  return 0;
+
+}
+
+#endif // Assign3_CPP
